3c2b91ce594b278fcf2b2b8dedef45355d541109
hashwanth.alla <hashwanth.alla@servicenow.com>
2026-06-03 17:58:42 +0530

    PYTHON: day 1 of python learning

--- PycharmProjects/100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
+++ PycharmProjects/100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -1,2 +1,7 @@
-# Write your code below this line 👇
-print("Hello World!")
+name = "Bhanu"
+print(name)
+name = "Hashu"
+print(name)
+username = input("What is your name? ")
+length = len(username)
+print(length)
